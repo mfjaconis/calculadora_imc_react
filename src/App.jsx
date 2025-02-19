60ec60147fd680464c9a24b31e8a4a4950ec18e6
mfjaconis <matheusjaconis@gmail.com>
2025-02-19 16:07:52 -0300

    estrutura do HTML para receber o formulário com o campo peso e altura

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 function App() {
-	return <></>;
+	return (
+		<>
+			<header>
+				<h1>Calculadora IMC</h1>
+			</header>
+			<main>
+				<form>
+					<input type="number" placeholder="Digite seu peso" />
+					<input type="number" placeholder="Digite sua altura" />
+					<button type="submit">Calcular</button>
+				</form>
+			</main>
+		</>
+	);
 }
 
 export default App;
